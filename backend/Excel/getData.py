1b5bec1adaab95603e8e74e28047d92ece8de7f3
Anubhav-Goyal01 <anubhavgoyal101@gmail.com>
2024-05-30 08:18:18 +0530

    Custom GetData Functions for MPA Australia

--- backend/Excel/getData.py
+++ backend/Excel/getData.py
@@ -21,6 +21,48 @@ def getData(credentials, scopes, site_id, drive_id, workbook_id, worksheet_id):
         print(f"Failed to retrieve worksheet data: {response.status_code}")
 
 
+def getDataMPAAustraliaMQ(credentials, scopes, site_id, drive_id, workbook_id, worksheet_id):
+    access_token = credentials.get_token(*scopes)
+    endpoint = f'https://graph.microsoft.com/v1.0/sites/{site_id}/drives/{drive_id}/items/{workbook_id}/workbook/worksheets/{worksheet_id}/usedRange'
+    headers = {
+        'Authorization': f'Bearer {access_token.token}'
+    }
+    response = requests.get(endpoint, headers=headers)
+    if response.status_code == 200:
+        worksheet_data = response.json()
+        data = worksheet_data['values']
+        first_empty_row = None
+        for i, row in enumerate(data):
+            if row[3]== "":
+                first_empty_row = i + 1
+                break
+        return first_empty_row, worksheet_data['values']
+    else:
+        print(f"Failed to retrieve worksheet data: {response.status_code}")
+
+
+def getDataMPAAustraliaTPQ(credentials, scopes, site_id, drive_id, workbook_id, worksheet_id):
+    access_token = credentials.get_token(*scopes)
+    endpoint = f'https://graph.microsoft.com/v1.0/sites/{site_id}/drives/{drive_id}/items/{workbook_id}/workbook/worksheets/{worksheet_id}/usedRange'
+    headers = {
+        'Authorization': f'Bearer {access_token.token}'
+    }
+    response = requests.get(endpoint, headers=headers)
+    if response.status_code == 200:
+        worksheet_data = response.json()
+        data = worksheet_data['values']
+        first_empty_row = None
+        for i, row in enumerate(data):
+            if i == 0:
+                continue
+            if row[3] == "":
+                first_empty_row = i + 1
+                break
+        return first_empty_row, worksheet_data['values']
+    else:
+        print(f"Failed to retrieve worksheet data: {response.status_code}")
+
+
 def getDataHeightPM(credentials, scopes, site_id, drive_id, workbook_id, worksheet_id):
     access_token = credentials.get_token(*scopes)
     endpoint = f'https://graph.microsoft.com/v1.0/sites/{site_id}/drives/{drive_id}/items/{workbook_id}/workbook/worksheets/{worksheet_id}/usedRange'
